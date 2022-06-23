7f90e02273a1b67f15aced09fa2713a240cdcae0
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-06-23 17:48:02 +0800

    change batch size match to img_list

--- run_mover.sh
+++ run_mover.sh
@@ -4,7 +4,7 @@
 # pid=$(printf "%06d" $1)
 # echo ${pid}
 # modify
-batch_size=100
+batch_size=1
 # img_list=`expr "$1" + "2"`
 img_list=$1
 #INPUT_DATA="/is/cluster/hyi/workspace/SceneGeneration/smplify-x_modify/debug/smplify_video_input/"
