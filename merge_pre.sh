2c6925f35051a15c0cfdc7dd34e8a3d91bf31e74
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-06 03:07:58 +0800

    test pare

--- merge_pre.sh
+++ merge_pre.sh
@@ -28,38 +28,38 @@ python demo.py \
        --vid_file ${video_file} \
        --draw_keypoints \
        --detector maskrcnn
-#
-#
-#DATA_FOLDER='/root/code/mover/preprocess/Color_flip/Color_flip_frames'
-#OUTPUT_FOLDER="/root/code/mover/preprocess/Color_flip_merge/OneEuro_filter_mv_smplifyx_input_withPARE"
-#JSON_FOLDER="/root/code/mover/preprocess/Color_flip/Color_flip_openpose"
-#pare_result='/root/code/mover/preprocess/Color_flip/pare_output.pkl'
-#cam_dir='/root/code/mover/smplifyx_cam'
-#
-#echo ${DATA_FOLDER}
-#echo ${OUTPUT_FOLDER}
-#
-#MODEL_FOLDER=/root/code/mover/preprocess/pare/data/body_models
-#VPOSER_FOLDER=/root/code/mover/smplifyx-file/vposer_v1_0
-#
-##cd /root/code/mover/preprocess/merge_preprocess/
-##source activate pare-env
-##source activate smplifyx
-#python demo_pare_result.py \
-#    --config ./cfg_files/fit_smpl.yaml \
-#    --export_mesh True \
-#    --save_new_json True \
-#    --json_folder  ${JSON_FOLDER} \
-#    --data_folder ${DATA_FOLDER} \
-#    --output_folder ${OUTPUT_FOLDER} \
-#    --pare_result ${pare_result} \
-#    --cam_dir ${cam_dir} \
-#    --visualize="False" \
-#    --model_folder ${MODEL_FOLDER} \
-#    --vposer_ckpt ${VPOSER_FOLDER} \
-#    --part_segm_fn /root/code/mover/smplifyx-file/smplx_parts_segm.pkl \
-#    --gender male \
-#    --check_inverse_feet="False" \
+
+
+DATA_FOLDER='/root/code/mover/preprocess/Color_flip/Color_flip_frames'
+OUTPUT_FOLDER="/root/code/mover/preprocess/Color_flip_merge/OneEuro_filter_mv_smplifyx_input_withPARE"
+JSON_FOLDER="/root/code/mover/preprocess/Color_flip/Color_flip_openpose"
+pare_result='/root/code/mover/preprocess/Color_flip/pare_output.pkl'
+cam_dir='/root/code/mover/smplifyx_cam'
+
+echo ${DATA_FOLDER}
+echo ${OUTPUT_FOLDER}
+
+MODEL_FOLDER=/root/code/mover/preprocess/pare/data/body_models
+VPOSER_FOLDER=/root/code/mover/smplifyx-file/vposer_v1_0
+
+#cd /root/code/mover/preprocess/merge_preprocess/
+#source activate pare-env
+#source activate smplifyx
+python demo_pare_result.py \
+    --config ./cfg_files/fit_smpl.yaml \
+    --export_mesh True \
+    --save_new_json True \
+    --json_folder  ${JSON_FOLDER} \
+    --data_folder ${DATA_FOLDER} \
+    --output_folder ${OUTPUT_FOLDER} \
+    --pare_result ${pare_result} \
+    --cam_dir ${cam_dir} \
+    --visualize="False" \
+    --model_folder ${MODEL_FOLDER} \
+    --vposer_ckpt ${VPOSER_FOLDER} \
+    --part_segm_fn /root/code/mover/smplifyx-file/smplx_parts_segm.pkl \
+    --gender male \
+    --check_inverse_feet="False" \
 
 
 cd /root/code/scripts
