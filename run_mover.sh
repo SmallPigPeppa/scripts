51b50d0373b7d845580804e3fd9d4df13bc54134
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-06 16:55:53 +0800

    run mover change batchsize

--- run_mover.sh
+++ run_mover.sh
@@ -4,7 +4,7 @@
 # pid=$(printf "%06d" $1)
 # echo ${pid}
 # modify
-batch_size=200
+batch_size=500
 # img_list=`expr "$1" + "2"`
 img_list=$1
 #INPUT_DATA="/is/cluster/hyi/workspace/SceneGeneration/smplify-x_modify/debug/smplify_video_input/"
